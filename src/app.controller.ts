deebb6bb2d2941886865aa465bd39ecec81bf762
baothi <baothi246@gmail.com>
2023-07-18 11:49:38 +0700

    UseGuards

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
@@ -9,13 +10,25 @@ export class AppController {
     private configService: ConfigService
     ) {}
 
-  @Get()
-  @Render("home")
-  getHello() {
-    console.log("check port = ", this.configService.get<string>("PORT"))
-    const mesage = this.appService.getHello();
-    return {
-      mesage: mesage
-    }
+  // @Get()
+  // @Render("home")
+  // getHello() {
+  //   console.log("check port = ", this.configService.get<string>("PORT"))
+  //   const mesage = this.appService.getHello();
+  //   return {
+  //     mesage: mesage
+  //   }
+  // }
+  
+  // @UseGuards(AuthGuard('local'))
+  // @Post('/login')
+  // handleLogin(@Request() req) {
+  //   return req.user;
+  // }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('/login')
+  handleLogin(@Request() req) {
+    return req.user;
   }
 }
